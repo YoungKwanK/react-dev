5286110b41428d9e51dc5c04d48ba069b3e489ff
YoungKwanK <young.kwin@gmail.com>
2025-11-24 12:36:11 +0900

    5장 Nested Routing

--- react-router-dom-example/src/index.js
+++ react-router-dom-example/src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import reportWebVitals from './reportWebVitals';
-import {Route, Routes, NavLink, HashRouter} from 'react-router-dom';
+import {Route, Routes, NavLink, HashRouter, useParams} from 'react-router-dom';
 
 function Home(){
   return(
@@ -13,18 +12,48 @@ function Home(){
   );
 }
 
-function Topics(){
+var contents = [
+  {id:1, title:'HTML', description:'HTML is ...'},
+  {id:2, title:'JS', description:'JS is ...'},
+  {id:3, title:'React', description:'React is ...'}
+];
+
+function Topic(){
+  var params = useParams();
+  var topic_id = params.topic_id;
+  var selected_topic = {
+    title:'Sorry',
+    description:'Not Found'
+  };
+  for(var i=0; i<contents.length; i++){
+    if(contents[i].id === Number(topic_id)){
+      selected_topic = contents[i];
+      break;
+    }
+  }
   return(
     <div>
+      <h3>{selected_topic.title}</h3>
+      {selected_topic.description}
+    </div>
+  );
+}
+
+function Topics(){
+  var lis = [];
+  for (var i=0; i<contents.length; i++){
+    lis.push(
+      <li key={contents[i].id}><NavLink to={"/topics/" + contents[i].id}>{contents[i].title}</NavLink></li>
+    );
+  }
+  return(
+    <div>
+      <h2>Topics</h2>
       <ul>
-        <li><NavLink to="/topics/1">HTML</NavLink></li>
-        <li><NavLink to='/topics/2'>JS</NavLink></li>
-        <li><NavLink to='/topics/3'>React</NavLink></li>
+        {lis}
       </ul>
       <Routes>
-        <Route path="/1" element={'HTML is ...'}/>
-        <Route path="/2" element={'JS is ...'}/>
-        <Route path="/3" element={'React is ...'}/>
+        <Route path="/:topic_id" element={<Topic />}/>
       </Routes>
     </div>
   );
@@ -58,8 +87,3 @@ function App(){
   );
 }
 ReactDOM.createRoot(document.getElementById('root')).render(<HashRouter><App /></HashRouter>);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
