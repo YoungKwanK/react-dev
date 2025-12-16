f40ce4904279a703d1ef9f5e4d9b0270ebe259ba
YoungKwanK <young.kwin@gmail.com>
2025-12-16 12:01:49 +0900

    Redux-toolkit 실습

--- react-redux-toolkit/src/App.js
+++ react-redux-toolkit/src/App.js
@@ -1,20 +1,25 @@
 import React from 'react';
 import {createStore} from 'redux';
 import {Provider, useSelector, useDispatch} from 'react-redux';
-function reducer(state, action){
-  if(action.type==='up'){
-    return {...state, value: state.value + action.step};
-  }
-  return state;
-}
-const initialState = {value:0};
-const store = createStore(reducer, initialState);
+import store from './store';
+import {up} from './counterSlice';
+// function reducer(state, action){
+//   if(action.type==='up'){
+//     return {...state, value: state.value + action.step};
+//   }
+//   return state;
+// }
+// const initialState = {value:0};
+// const store = createStore(reducer, initialState);
 function Counter() {
   const dispatch = useDispatch();
-  const count = useSelector(state=>state.value);
+  const count = useSelector((state)=>{
+    return state.counter.value}
+  );
   return <div>
     <button onClick={()=>{
-      dispatch({type:'up', step:2});
+      // dispatch({type:'counterSlice/up', step:2});
+      dispatch(up(2));
     }}>+</button>{count}
   </div>
 }
